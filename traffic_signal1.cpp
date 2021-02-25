463fa32b694e8eae77a0655745b05516f19cee03
shikharsun <shikhar21121999@gmail.com>
2021-02-26 00:48:35 +0530

    input done

---
+++ traffic_signal1.cpp
@@ -0,0 +1,52 @@
+// program to solve hashcode version 1
+
+#include <bits/stdc++.h>
+using namespace std;
+# define space " "
+# define pii pair<int,int>
+// D is duration
+// I intersections
+// S is number of streets
+// V is number of cars
+// F bonus points for each car that reaches the destination
+
+int main(){
+	int D,I,S,V,F;
+	cin>>D>>I>>S>>V>>F;
+	
+	// create a graph with I vertices
+	vector <vector <int> > G(I+1,vector <int> (I+1,0));
+	
+	// make a map to store names of streets between two intersections
+	unordered_map <string,pii> street_name;
+	
+	// input the streets
+	int B,E,L;
+	string name;
+	for(int i=0;i<S;i++){
+		cin>>B>>E>>name>>L;
+		G[B][E]=L;
+		street_name[name]=make_pair(B,E);
+	}
+	
+	// store path of cars as a vector of vector of strings
+	vector<vector <string> > cars_path;
+	// can use reserve here
+	
+	// path of each car
+	int streets;
+	for(int i=0;i<V;i++){
+		cin>>streets;
+		string str_name;
+		vector <string> path;
+		for(int j=0;j<streets;j++){
+			cin>>str_name;
+			path.push_back(str_name);
+		}
+		cars_path.push_back(path);
+	}
+	
+}
+		
+	
+	
